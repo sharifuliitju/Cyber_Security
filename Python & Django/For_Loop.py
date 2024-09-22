0093cd067dd0cbf9d51ccb7330a9c465cdf53058
Shariful Islam <94281308+sharifuliitju@users.noreply.github.com>
2024-09-22 22:46:17 +0600

    For_loop

---
+++ Python & Django/For_Loop.py
@@ -0,0 +1,22 @@
+# fruits = ["apple","banana","cherry"]
+# for x in fruits:
+#     print(x)
+
+
+# for p in 'Sharif':
+#     print(p)
+
+
+# for q in 'Sharif':
+#     # print(q)
+#     if(q == "i"):
+#         break  #continue
+#     print(q)
+
+
+fruits = ["apple","banana","cherry"]
+for x in fruits:
+    print(x)
+    # break
+else:
+    print("No fruits left")
